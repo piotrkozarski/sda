afc2e15365e5203bdfcbb066ea0ed0fee811608a
piotrkozarski <pk.mejl@gmail.com>
2018-07-19 19:50:25 +0200

    streamy commit 4

--- StreamTest/src/main/java/StreamTest/PersonList.java
+++ StreamTest/src/main/java/StreamTest/PersonList.java
@@ -1,21 +1,27 @@
 package StreamTest;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class PersonList {
 
 
     public static void main(String[] args) {
-        List<Persons> personsList = Arrays.asList(new Persons("jan"),
-                new Persons("anna"),
-                new Persons("zenon"));
+        List<Persons> personsList = new ArrayList<>(Arrays.asList(new Persons("jan", 17),  //to new ArrayList musi być dodane,
+                new Persons("anna", 20),                                                    // mimo że asList samo wywołuje new ArrayList,
+                new Persons("zenon", 23)));                                                 // ale bez tego sie wywala, szczegoly na stakoverflon
         personsList.stream()
                 .collect(Collectors.toList())
                 .forEach(name -> System.out.println(name));
+        System.out.println();
 
 
+        Predicate<Persons> personsPredicate = (persons -> (persons.getAge() < (18)) || (persons.getAge() < 21 && persons.getName().endsWith("a")));
+        personsList.removeIf(personsPredicate);
+        personsList.forEach(persons -> System.out.println(persons));
 
     }
 }
--- StreamTest/src/main/java/StreamTest/Persons.java
+++ StreamTest/src/main/java/StreamTest/Persons.java
@@ -6,13 +6,35 @@ import java.util.List;
 
 class Persons {
     String name;
+    int age;
 
-    public Persons(String name) {
+    public Persons(String name, int age) {
         this.name = name;
+        this.age = age;
 
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Persons{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 }

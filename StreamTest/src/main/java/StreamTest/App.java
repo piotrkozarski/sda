c87eac1431e4995e425baea785532f9be0f48b2c
piotrkozarski <pk.mejl@gmail.com>
2018-07-19 18:02:18 +0200

    test streama commit 1

---
+++ StreamTest/src/main/java/StreamTest/App.java
@@ -0,0 +1,13 @@
+package StreamTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
